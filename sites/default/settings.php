9c0689d144b96df644e6d3de6d643254e5517eb2
Connor Abdelnoor <connor.abd@gmail.com>
2015-05-22 14:08:04 -0700

    Add custom built cipher module to drupal page.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'yourname_cipher',
+      'username' => 'epicodus',
+      'password' => 'epicodus',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'rw-wikRVSUUGQUZ-fBuglVMGnvxWsNVK0NnGOI-BYKw';
 
 /**
  * Base URL (optional).
